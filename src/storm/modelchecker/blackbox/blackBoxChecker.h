b339e18af4a5bafdbe2fb05c5708aefc4efb6a41
Roman Lezovic <roman.lezovic02@gmail.com>
2023-06-09 00:05:57 +0200

    add namespace blackbox checker

--- src/storm/modelchecker/blackbox/blackBoxChecker.h
+++ src/storm/modelchecker/blackbox/blackBoxChecker.h
@@ -5,6 +5,15 @@
 #ifndef STORM_BLACKBOXCHECKER_H
 #define STORM_BLACKBOXCHECKER_H
 
+#include "storm/modelchecker/AbstractModelChecker.h"
+
+namespace storm {
+namespace modelchecker {
+namespace blackbox {
+    
 class blackBoxChecker {};
 
+} //namespace blackbox
+} //namespace modelchecker
+} //namespace storm
 #endif  // STORM_BLACKBOXCHECKER_H
