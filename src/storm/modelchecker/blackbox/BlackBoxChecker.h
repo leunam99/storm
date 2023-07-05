df567d01c9623766bb7c45ba5861e6e9fa8096a6
Roman Lezovic <roman.lezovic02@gmail.com>
2023-07-05 22:01:27 +0200

    setup header BlackBoxChecker

--- src/storm/modelchecker/blackbox/BlackBoxChecker.h
+++ src/storm/modelchecker/blackbox/BlackBoxChecker.h
@@ -5,6 +5,42 @@
 #ifndef STORM_BLACKBOXCHECKER_H
 #define STORM_BLACKBOXCHECKER_H
 
-class BlackBoxChecker {};
+#include "storm/modelchecker/AbstractModelChecker.h"
+#include "storm/modelchecker/blackbox/EMdp.h"
+#include "storm/modelchecker/blackbox/BMdp.h"
 
+
+namespace storm {
+namespace modelchecker {
+namespace blackbox {
+
+template<typename ModelType, typename StateType>
+class BlackBoxChecker: public storm::modelchecker::AbstractModelChecker<ModelType> {
+    public:
+     typedef typename ModelType::ValueType ValueType;
+     typedef double BoundType;  // TODO can/should we declare this in some other way? template?
+
+     BlackBoxChecker(storm::prism::Program const& program);
+
+     /*!
+      * Determines whether the model checker can handle the given verification task. If this method returns
+      * false, the task must not be checked using this model checker.
+      *
+      * @param checkTask The task for which to check whether the model checker can handle it.
+      * @return True iff the model checker can check the given task.
+      */
+     bool canHandle(CheckTask<storm::logic::Formula, ValueType> const& checkTask);
+     
+     std::unique_ptr<CheckResult> computeUntilProbabilities(Environment const& env, CheckTask<storm::logic::UntilFormula, ValueType> const& checkTask);
+
+    private:
+     std::shared_ptr<BlackboxMDP<StateType>> blackboxMDP;
+     EMdp<StateType> eMDP;
+     BMdp<BoundType> bMDP;
+
+};
+
+} //namespace blackbox
+} //namespace modelchecker
+} //namespace storm
 #endif  // STORM_BLACKBOXCHECKER_H
