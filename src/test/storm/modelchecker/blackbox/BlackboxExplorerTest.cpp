aa7d896cbb758b3ae0257b24ca379d581bb18813
Roman Lezovic <roman.lezovic02@gmail.com>
2023-08-01 19:07:04 +0200

    update explorer tets

--- src/test/storm/modelchecker/blackbox/BlackboxExplorerTest.cpp
+++ src/test/storm/modelchecker/blackbox/BlackboxExplorerTest.cpp
@@ -7,7 +7,7 @@
 #include "storm/modelchecker/blackbox/heuristic-simulate/HeuristicSim.h"
 
 
-TEST(BlackBoxExplorer, exploreSimpleMDP) {
+TEST(BlackBoxExplorer, exploreSimplePath) {
     storm::prism::Program program = storm::parser::PrismParser::parse(STORM_TEST_RESOURCES_DIR "/mdp/two_dice.nm");
     std::seed_seq seed({0});
     auto blackboxMDP = std::static_pointer_cast<storm::modelchecker::blackbox::BlackboxMDP<uint32_t, double>>(std::make_shared<storm::modelchecker::blackbox::BlackboxWrapperOnWhitebox<uint32_t, double>>(program));
@@ -17,17 +17,10 @@ TEST(BlackBoxExplorer, exploreSimpleMDP) {
     storm::modelchecker::blackbox::EMdp<uint32_t> eMDP;
     explorer.performExploration(eMDP, 1);
 
-    // expect a single was sampled of maximal 5 different states, more is not possible in two dice
-    ASSERT_EQ(eMDP.getTotalStateCount(), 5);
-    std::cout << "state COUNT" << eMDP.getTotalStateCount() << "\n";
-    std::vector<uint32_t> states = {0, 1, 2, 3, 4, 5};
-
-    ASSERT_EQ(eMDP.getSampleCount(0, 0, 1), 1);
-    std::cout << "state COUNT" << eMDP.getTotalStateCount() << "\n";
-
-    ASSERT_EQ(eMDP.getSampleCount(1, 0, 2), 1);
-    if (eMDP.getSuccCount(2, 0) > 1) {
-        // exploration looped in states 
+    // simply expect a path i -> (i+1), each one sampled one time, last one is not checked as it can be a sink state with arbitrary self loops
+    uint32_t total_states = eMDP.getTotalStateCount();
+    for (uint32_t i = 0; i < eMDP.getTotalStateCount() - 2; i++) {
+        ASSERT_EQ(eMDP.getSampleCount(i, 0, i+1), 1);
+        ASSERT_EQ(eMDP.getSampleCount(i, 0), 1);
     }
 }
-
